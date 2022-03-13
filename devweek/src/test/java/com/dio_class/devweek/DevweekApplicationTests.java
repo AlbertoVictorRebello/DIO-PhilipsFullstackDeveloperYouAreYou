fd40de8d079e4e55396b44619b44248c63610102
albertoVictorRebello <albertorebello@gmail.com>
2022-03-13 00:41:21 -0300

    develop tests

--- devweek/src/test/java/com/dio_class/devweek/DevweekApplicationTests.java
+++ devweek/src/test/java/com/dio_class/devweek/DevweekApplicationTests.java
@@ -1,9 +1,11 @@
 package com.dio_class.devweek;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DevweekApplicationTests {
 
 	@Test
