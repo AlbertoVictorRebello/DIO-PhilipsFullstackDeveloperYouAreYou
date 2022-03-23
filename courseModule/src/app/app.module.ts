8c382f25ca32e7d0975df40fb61ab4d8e7a15449
albertoVictorRebello <albertorebello@gmail.com>
2022-03-23 15:58:40 -0300

    develop routing feature

--- courseModule/src/app/app.module.ts
+++ courseModule/src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '', redirectTo: 'courses', pathMatch: 'full'
+        },
+        {
+          path: 'courses', component: CourseListComponent        }
+      ]
+    ),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
