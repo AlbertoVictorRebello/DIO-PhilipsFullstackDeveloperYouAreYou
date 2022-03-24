096d14352792b5b1600a222a924b302d48d17dda
albertoVictorRebello <albertorebello@gmail.com>
2022-03-24 04:08:02 -0300

    implemented segregated course module

--- courseModule/src/app/app.module.ts
+++ courseModule/src/app/app.module.ts
@@ -1,43 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+
 import { RouterModule } from '@angular/router';
 import { HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
+import { CourseModule } from './courses/course.module';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
+    AppComponent,  
+        
     NavBarComponent,
     error404Component,
-    CourseInfoComponent
+    
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule,    
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot(
-      [
-        {
-          path: 'courses', component: CourseListComponent
-        },
-        
-        {
-          path: 'courses/info/:id', component: CourseInfoComponent
-        },
-        
+      [              
         {
           path: '', redirectTo: 'courses', pathMatch: 'full'
         },
