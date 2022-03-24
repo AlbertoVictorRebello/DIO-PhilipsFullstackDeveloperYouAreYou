e3b90014f9a0e3fb321c1e7604f3ba4bc75e342c
albertoVictorRebello <albertorebello@gmail.com>
2022-03-24 04:25:57 -0300

    implemented shared module

--- courseModule/src/app/courses/course.module.ts
+++ courseModule/src/app/courses/course.module.ts
@@ -2,21 +2,21 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from '@angular/router';
-import { ReplacePipe } from "../pipe/replace.pipe";
-import { StarComponent } from "../star/star.component";
+import { StarModule } from "../shared/component/star/star.module";
 import { CourseInfoComponent } from "./course-info.component";
 import { CourseListComponent } from "./course-list.component";
+import { AppPipeModule } from "../shared/component/pipe/app-pipe-module";
 
 @NgModule({
     declarations:[
         CourseListComponent,
-        CourseInfoComponent,
-        ReplacePipe,
-        StarComponent
+        CourseInfoComponent    
     ],
     imports:[
         CommonModule,
         FormsModule,
+        StarModule,
+        AppPipeModule,
         RouterModule.forChild([
             {
                 path: 'courses', component: CourseListComponent
