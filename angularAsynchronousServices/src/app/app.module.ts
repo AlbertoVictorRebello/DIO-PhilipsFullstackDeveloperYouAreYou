0495d79bfd341de9bd64e1856ae15d00d6145921
albertoVictorRebello <albertorebello@gmail.com>
2022-03-30 02:43:04 -0300

    created users list component

--- angularAsynchronousServices/src/app/app.module.ts
+++ angularAsynchronousServices/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersListComponent
   ],
   imports: [
     BrowserModule,
